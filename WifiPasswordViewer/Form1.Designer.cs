b02f1ab086ebc915d490f1c122a065d0d9db8ab5
Udesh Bandara <ukmihiran@gmail.com>
2022-05-17 07:00:53 +0100

    Add project files.

---
+++ WifiPasswordViewer/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WifiPasswordViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnViewPass = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.WifiName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Password = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnViewPass
+            // 
+            this.btnViewPass.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnViewPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewPass.ForeColor = System.Drawing.Color.White;
+            this.btnViewPass.Location = new System.Drawing.Point(413, 267);
+            this.btnViewPass.Name = "btnViewPass";
+            this.btnViewPass.Size = new System.Drawing.Size(103, 35);
+            this.btnViewPass.TabIndex = 0;
+            this.btnViewPass.Text = "Get Password";
+            this.btnViewPass.UseVisualStyleBackColor = false;
+            this.btnViewPass.Click += new System.EventHandler(this.btnViewPass_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.Default;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.WifiName,
+            this.Password});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView1.Location = new System.Drawing.Point(16, 60);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.ShowItemToolTips = true;
+            this.listView1.Size = new System.Drawing.Size(500, 201);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyUp);
+            // 
+            // WifiName
+            // 
+            this.WifiName.Text = "WiFi Name";
+            this.WifiName.Width = 250;
+            // 
+            // Password
+            // 
+            this.Password.Text = "Password";
+            this.Password.Width = 250;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "WiFi Password Viewer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Version: 1.0.0 by ukmihiran |";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(153, 304);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(52, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "ukmihiran";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // btnExport
+            // 
+            this.btnExport.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.ForeColor = System.Drawing.Color.White;
+            this.btnExport.Location = new System.Drawing.Point(304, 267);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(103, 35);
+            this.btnExport.TabIndex = 5;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = false;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 329);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnViewPass);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WiFi Password Viewer ~ By ukmihiran";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnViewPass;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader WifiName;
+        private System.Windows.Forms.ColumnHeader Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ WifiPasswordViewer/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WifiPasswordViewer
+{
+    public partial class Form1 : Form
+    {
+        private int count;
+        private int count_names;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Get all WiFi list 
+        private string getWifiList()
+        {
+            Process processWiFi = new Process();
+            processWiFi.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            processWiFi.StartInfo.FileName = "netsh";
+            processWiFi.StartInfo.Arguments = "wlan show profile";
+            processWiFi.StartInfo.UseShellExecute = false;
+            processWiFi.StartInfo.RedirectStandardError = true;
+            processWiFi.StartInfo.RedirectStandardInput = true;
+            processWiFi.StartInfo.RedirectStandardOutput = true;
+            processWiFi.StartInfo.CreateNoWindow = true;
+            processWiFi.Start();
+
+            string data = processWiFi.StandardOutput.ReadToEnd();
+            string error = processWiFi.StandardError.ReadToEnd();
+            processWiFi.WaitForExit();
+            return data;
+        }
+        
+        //read WiFi Password
+        private string readPassword(string WiFiName) 
+        {
+            string argument = "wlan show profile name=\"" + WiFiName + "\" key=clear";
+            Process processWifi = new Process();
+            processWifi.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            processWifi.StartInfo.FileName = "netsh";
+            processWifi.StartInfo.Arguments = argument;
+
+            processWifi.StartInfo.UseShellExecute = false;
+            processWifi.StartInfo.RedirectStandardError = true;
+            processWifi.StartInfo.RedirectStandardInput = true;
+            processWifi.StartInfo.RedirectStandardOutput = true;
+            processWifi.StartInfo.CreateNoWindow = true;
+            processWifi.Start();
+
+            string data = processWifi.StandardOutput.ReadToEnd();
+            string error = processWifi.StandardError.ReadToEnd();
+
+            processWifi.WaitForExit();
+
+            return data;
+        }
+
+        //Get Password
+        private string getPassword(string WiFiName)
+        {
+            string get_pass = readPassword(WiFiName);
+            using (StringReader reader = new StringReader(get_pass))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Regex regex = new Regex(@"Key Content * : (?<after>.*)");
+                    Match match = regex.Match(line);
+
+                    if (match.Success)
+                    {
+                        string current_password = match.Groups["after"].Value;
+                        return current_password;
+                    }
+                }
+            }
+            return "Open WiFi";
+        }
+
+        //Get WiFi Password
+        private void getWiFiPassword()
+        {
+            string WifiPass = getWifiList();
+            using (StringReader reader = new StringReader(WifiPass))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    count++;
+                    Regex regex = new Regex(@"All User Profile * : (?<after>.*)");
+                    Match match = regex.Match((line));
+
+                    if (match.Success)
+                    {
+                        count_names++;
+                        string WiFi_Name = match.Groups["after"].Value;
+                        string Password = getPassword(WiFi_Name);
+                        listView1.Items.Add(WiFi_Name).SubItems.Add(Password);
+
+                    }
+                }
+            }
+        }
+
+        private void btnViewPass_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            btnViewPass.Enabled = false;
+            getWiFiPassword();
+            btnViewPass.Enabled = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Specify that the link was visited.
+            this.linkLabel1.LinkVisited = true;
+
+            // Navigate to a URL.
+            System.Diagnostics.Process.Start("https://github.com/ukmihiran");
+        }
+
+        //Copy Password from ListView
+        private void listView1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (sender != listView1) { return; }
+
+            if (e.Control && e.KeyCode == Keys.C)
+                CopySelectedValues();
+        }
+        private void CopySelectedValues()
+        {
+            var builder = new StringBuilder();
+            foreach (ListViewItem item in listView1.SelectedItems)
+                builder.AppendLine(item.SubItems[1].Text);
+
+            Clipboard.SetText(builder.ToString());
+        }
+
+        //Export Data
+        private async void btnExport_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = "Text Documents |*.txt", ValidateNames = true })
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    using (TextWriter writer = new StreamWriter(new FileStream(saveFileDialog.FileName, FileMode.Create), Encoding.UTF8))
+                    {
+                        foreach(ListViewItem item in listView1.Items)
+                        {
+                            await writer.WriteLineAsync(item.SubItems[0].Text + "\t" + item.SubItems[1].Text);
+                        }
+                        MessageBox.Show("Your data has been successfully exported.","Message",MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+            }
+        }
+    }
+}
